28d2b2c3333c0e962becc1c892586cda1e399618
Bryan Leighton <bryan.leigh77@gmail.com>
2019-02-19 17:30:54 +0100

    mutation.js of backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,36 +1,36 @@
 const Mutations = {
- async createItem(parent,args,ctx,info){
-    
+  async createItem(parent, args, ctx, info) {
     // TODO Check if they are logged in
-    // ctx is established in the createServer.. 
+    // ctx is established in the createServer..
     // ctx is a promise.. so async/await for it to be resolved
     // info has the actual query.. so stx.database.mutation needs to be passed to back-end
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
       },
-    }, info);
+      info
+    );
 
     return item;
   },
-  updateItem(parent, args, ctx, info){
+  updateItem(parent, args, ctx, info) {
     const updates = { ...args }; // take copy of all data in updates
-    delete updates.id
+    delete updates.id;
     // remove the id from the updates as to NOT update the id
     // RUN UPDATE METHOD
     // ctx -> context in request
-    // db -> expose Prisma DB, mutation.. 
-   return ctx.db.mutation.updateItem({
+    // db -> expose Prisma DB, mutation..
+    return ctx.db.mutation.updateItem({
       data: updates,
-      where: { 
+      where: {
         id: args.id
       },
-       info  
-       /* updateItem is expecting us to return an item so 'info' is 
+      info
+      /* updateItem is expecting us to return an item so 'info' is 
        reference to query that contains that item */
-   })
-  } 
-    
+    });
+  }
 };
 module.exports = Mutations;
-
