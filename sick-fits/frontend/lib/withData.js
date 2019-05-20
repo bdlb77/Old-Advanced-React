2c93abad76c6a95308388e5dfc838b5891afbac5
Bryan Leighton <bryan.leigh77@gmail.com>
2019-05-20 17:34:43 +0300

    apollo cart clientside

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -3,21 +3,47 @@
 import withApollo from 'next-with-apollo';
 // apollo-boost is a package that contains many boilerplate packages of Apollo.. 1 stop shop essentially
 import ApolloClient from 'apollo-boost';
+import { LOAD_LOCAL_STATE_QUERY } from '../components/Cart';
 import { endpoint } from '../config';
 
 function createClient({ headers }) {
-  return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    // on every request... include the credentials -> if any logged-in cookies.. it'll be brought along
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include',
-        },
-        headers,
-      });
-    },
-  });
+	return new ApolloClient({
+		uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+		// on every request... include the credentials -> if any logged-in cookies.. it'll be brought along
+		request: operation => {
+			operation.setContext({
+				fetchOptions: {
+					credentials: 'include',
+				},
+				headers,
+			});
+		},
+		// Local Data
+		clientState: {
+			resolvers: {
+				Mutation: {
+					toggleCart(_, variables, { cache }) {
+						// read the cache
+						const { cartOpen } = cache.readQuery({
+							query: LOAD_LOCAL_STATE_QUERY,
+						});
+						// take out carOpen.. flip it.
+						const data = {
+							data: {
+								cartOpen: !cartOpen,
+							},
+						};
+						// put back in cache
+						cache.writeData(data);
+						return data;
+					},
+				},
+			},
+			defaults: {
+				cartOpen: true,
+			},
+		},
+	});
 }
 // createClient -> done in App.js
 export default withApollo(createClient);
