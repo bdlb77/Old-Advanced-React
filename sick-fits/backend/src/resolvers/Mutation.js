a56ee4975f0dea7c456ff1d091934fc3d36995db
Bryan Leighton <bryan.leigh77@gmail.com>
2019-05-27 12:20:15 +0300

    all new things for today

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -249,6 +249,21 @@ const Mutations = {
 			info
 		);
 	},
+	async removeFromCart(parent, args, ctx, info) {
+		// 1. query cart item
+		const cartItem = await ctx.db.query.cartItem(
+			{
+				where: { id: args.id },
+			},
+			`{id, user { id }}`
+		);
+		// 2. Make no cartItem found
+		if (!cartItem) throw new Error('No Cart Item Found!');
+		// 2. make sure they own cart item
+		if (cartItem.user.id !== ctx.request.userId) throw new Error('Your Are Not the Owner!');
+		// 3. delete that cart Item
+		return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
+	},
 };
 
 module.exports = Mutations;
